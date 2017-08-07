cd0d94e92b1f3c92410a2ffc8e0f9e3b73464252
SS <ss@successivesoftwares.com>
2017-08-08 00:42:31 +0530

    socket .io version updated

--- config/express.js
+++ config/express.js
@@ -1,8 +1,8 @@
 module.exports = function(express, app, server, settings, passport) {
 	server.listen(settings.port)
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(express.session({secret: settings.sessionSecret}));
+	//app.use(express.bodyParser());
+	//app.use(express.methodOverride());
+	//app.use(express.session({secret: settings.sessionSecret}));
 	app.use(passport.initialize());
 	app.use(passport.session());	
 };
