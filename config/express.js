8990e3e60fd5ed80f29c19bfbb0c302ab8e2fcca
nxneha <neha@nodexperts.com>
2019-06-02 12:46:02 +0530

    removed commented code

--- config/express.js
+++ config/express.js
@@ -1,8 +1,5 @@
 module.exports = function(express, app, server, settings, passport) {
 	server.listen(settings.port)
-	//app.use(express.bodyParser());
-	//app.use(express.methodOverride());
-	//app.use(express.session({secret: settings.sessionSecret}));
 	app.use(passport.initialize());
 	app.use(passport.session());	
 };
